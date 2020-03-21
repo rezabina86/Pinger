16459cd18a129eaf6039d8c23b57a9bfdc32e6e4
Reza Bina <tavakolibina@yahoo.com>
2020-03-21 10:22:18 +0200

    Initial Commit

---
+++ Pinger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pinger
+//
+//  Created by Reza Bina on 2020-03-21.
+//  Copyright © 2020 Reza Bina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
